8e83dcf8f206d2681cc4cf49b254c355e6ea9d61
Maryna Migacheva <m.migacheva@gmail.com>
2020-09-30 09:00:13 +0300

    feat: add three functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,52 @@
 
 exports.min = function min (array) {
-  return 0;
+	if(array === undefined) {
+		return 0;
+		} else {
+			let n = array.length;
+			if(n == 0) {
+				return 0;
+			} else {
+				let min=array[0];
+				for(let i=1; i<n; i++) {
+					if(array[i]<min) {min=array[i];}
+				}
+				return min;
+			}
+		}
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array === undefined) {
+		return 0;
+		} else {
+			let n = array.length;
+			if(n == 0) {
+				return 0;
+			} else {
+				let max=array[0];
+				for(let i=1; i<n; i++) {
+					if(array[i]>max) {max=array[i];}
+				}
+				return max;
+			}
+		}
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array === undefined) {
+		return 0;
+		} else {
+			let n = array.length;
+			if(n == 0) {
+				return 0;
+			} else {
+				let sum = 0;
+				for(let i=0; i<n; i++) {
+					sum += array[i];
+				}
+				let avg = sum/n;
+				return avg;
+			}
+		}
 }
